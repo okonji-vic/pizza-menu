67ce20ef39d4660f5ac4a56e61c06bdb711fb44e
Okonji Emeka <65029583+okonji-vic@users.noreply.github.com>
2025-01-06 22:37:24 +0100

    header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 function Header() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
     return (
         <>
             <div className="header-container">
@@ -14,7 +20,12 @@ function Header() {
                     <p className="brand-name">Pizzalleria</p>
                 </div>
                 <nav className="navigation">
-                    <ul className="nav-links">
+                    <div className="hamburger" onClick={toggleNav}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <ul className={`nav-links ${isNavOpen ? "active" : ""}`}>
                         <li><a href="#menu">Menu</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#gallery">Gallery</a></li>
