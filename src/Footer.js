5e623a1bdaacf55341caa93cbc0b5e1cca2c503e
Okonji Emeka <65029583+okonji-vic@users.noreply.github.com>
2025-01-07 12:33:13 +0100

    greeting

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             {/* <p className="p2">Click on the pizza to see the ingredients</p> */}
             
             {new Date().getHours() >= 12 && new Date().getHours() <= 18? <p className="p3">Good afternoon</p> : null}
-            {new Date().getHours() <= 12 ? <p className="p3">Good morning</p> : null}
+            {new Date().getHours() < 12 ? <p className="p3">Good morning</p> : null}
             {new Date().getHours() >= 18 && new Date().getHours() < 23? <p className="p3">Good evening</p> : null}
             {new Date().getHours() >= 23 ? <p className="p3">Good night</p> : null}
             {new Date().getHours() >= 11 && new Date().getHours() < 23 ? <p className="p3">We are currently open</p> : <p className="p3">We are currently closed. Reopens 11:00 - 23:00</p>}
