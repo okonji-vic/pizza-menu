12dee1034e9d509408c5e1d3620d1bd7be8a2541
Okonji Emeka <65029583+okonji-vic@users.noreply.github.com>
2025-01-07 18:03:44 +0100

    unused var removed

--- src/Pizzamenu.js
+++ src/Pizzamenu.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Pizzamenu.css";
-import Header from "./Header";
-import Footer from "./Footer";
 import Filter from "./Filter";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
